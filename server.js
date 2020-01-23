7e335316db0f29b42fc5a802141dfbd201b63c14
educedesign_markus <32208301+educedesign@users.noreply.github.com>
2020-01-23 10:19:37 -0800

    Added comments to server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require('express');
+var express = require('express'); // load express into a variable called 'express'
 
-var app = express();
-var server = app.listen(3000);
+var app = express(); // store the express function inside 'app'
+var server = app.listen(3000); // tell the app to listen to port 3000 and store the result inside 'server'
 
-app.use(express.static('public'));
+app.use(express.static('public')); // tell the app to use express and serve the static files inside 'public'
 
-console.log('server is running');
+console.log('server is running'); // console log to indicate that the server is running
