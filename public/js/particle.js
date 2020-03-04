d926174f13c90a5ff1886eadeadb10590469c14b
eli muro <emuro15@gmail.com>
2020-03-03 19:19:54 -0800

    Add files via upload
    added cloudy function tp particle.js
    bound data to weather info side bar

--- public/js/particle.js
+++ public/js/particle.js
@@ -5,12 +5,13 @@ class Particle {
         this.id = n; // set the ID of the particle
         this.setParticleMode(0); // call the wait function
         this.mode; // variable to store current mode
+
     }
 
     setParticleMode(m) {
         this.mode = m;
 
-        switch(this.mode) {
+        switch (this.mode) {
             case 0:
                 this.wait();
                 break;
@@ -23,6 +24,9 @@ class Particle {
             case 3:
                 this.wind();
                 break;
+            case 4:
+                this.cloudy();
+                break;
             default:
                 this.wait();
                 break;
@@ -32,11 +36,18 @@ class Particle {
     wait() {
         // init function which sets the starting point of the particle and a random direction
         //console.log("wait mode");
+        this.dia = 5; // set the diameter of the particle
+
         let x = (appWidth/14) * (this.id%14) + ((appWidth/14)/2); // calculate the x position of the particle in the grid based on it's id and the width of the canvas
         let y = (appHeight/14)*(floor(this.id/14)) + ((appHeight/14)/2); // calculate the y position of the particle in the grid based in it's id and height of the canvas
 
-        this.pos = createVector(x,y); // particles are arranged in a grid (using the previously calculated x and y position)
-        this.dir = createVector(0,0); // no direction needed for wait mode
+        
+
+
+        this.pos = createVector(x, y);
+
+        //this.pos = createVector(x,y); // particles are arranged in a grid (using the previously calculated x and y position)
+        this.dir = createVector(0, 0); // no direction needed for wait mode
         this.acc = 0; // no acceleration needed for wait mode
 
         this.age = 0; // no age needed for wait mode
@@ -46,45 +57,62 @@ class Particle {
     sun() {
         // function to execute when the weather is sunny which emits the points from the center into a random direction
         //console.log("sun mode");
-        this.pos = createVector(appWidth/2, appHeight/2); // put particle to 0/0
+        this.dia = 5; // set the diameter of the particle
+
+        this.pos = createVector(appWidth / 2, appHeight / 2); // put particle to 0/0
         this.dir = p5.Vector.random2D(); // create a random direction and store it in 'dir'
         this.dir.mult(25); // multiple 'dir' by 25
         this.pos.add(this.dir); // offset 'pos' of the particle by 'dir' from the center
 
         this.dir.normalize(); // normalize 'dir' so subsequent steps are smaller
-        this.acc = random(.5,1); // create a random number between 1 and 3 for the particles acceleration and store it in 'acc'
+        this.acc = random(.5, 1); // create a random number between 1 and 3 for the particles acceleration and store it in 'acc'
         this.dir.mult(this.acc); // multiply 'dir' with 'acc'
 
         this.age = 0; // set the age of the particle to 0
-        this.lifespan = random(55,233); // each particle receives a random lifespan between 100 and 200 frames
+        this.lifespan = random(55, 233); // each particle receives a random lifespan between 100 and 200 frames
     }
 
     rain() {
         // function to execute when the weather is rainy
         //console.log("rain mode");
-        this.pos = createVector(random(0,appWidth), random(0,appHeight));
+        this.dia = 5; // set the diameter of the particle
+        this.pos = createVector(random(0, appWidth), random(0, appHeight));
         this.dir = createVector(0, 1);
-        this.acc = random(.5,1);
+        this.acc = random(.5, 1);
         this.dir.mult(this.acc);
         this.age = 0;
-        this.lifespan = random(55,233);
+        this.lifespan = random(55, 233);
     }
 
     wind() {
         // function to execute when the weather is windy
         //console.log("wind mode");
+        this.dia = 5; // set the diameter of the particle
         this.pos = createVector(random(0, appWidth), random(0, appHeight));
-        this.dir = createVector(1,0);
+        this.dir = createVector(1, 0);
         this.acc = random(.5, 1);
         this.dir.mult(this.acc);
         this.age = 0;
-        this.lifespan = random(55,233);
+
+        this.lifespan = random(55, 233);
+    }
+
+    cloudy() {
+        // function to execute when the weather is cloudy
+        //console.log("wind mode");
+        this.dia = Math.floor(Math.random() * 200);
+        this.pos = createVector(random(0, appWidth), random(0, appHeight));
+        this.dir = createVector(1, 0);
+        this.acc = random(.5, .2);
+        this.dir.mult(this.acc);
+        this.age = 0;
+        this.lifespan = random(55, 3000);
     }
 
     update() {
         // update function increases the position of the particle and checks if it's lifespan is over
-        if(this.lifespan != 0) {
-            if(this.age < this.lifespan) { // if the age is below the lifespan the particle lives and moves
+        if (this.lifespan != 0) {
+            if (this.age < this.lifespan) { // if the age is below the lifespan the particle lives and moves
                 this.pos.add(this.dir); // add 'dir' to 'pos' (particle is moving)
                 this.age++; // age of the particle increases by 1
                 //console.log("update");
@@ -97,7 +125,7 @@ class Particle {
     }
 
     show() {
-        fill(255); // particles are drawn with a black fill color
+        fill(255); // particles are drawn with a white fill color
         noStroke(); // particels are drawn without a stroke
         ellipseMode(CENTER); // particles are drawn from the center
         ellipse(this.pos.x, this.pos.y, this.dia, this.dia); // we draw an ellipse using the particle's position and diameter
--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,17 +1,18 @@
 var appDiv; // variable to store the div containing the app
+
 var p5AppCanvas; // variable to store the p5 canvas
 var appWidth; // variable to store the width of the div holding the visualization
 var appHeight; // variable to store the height of the div holding the visualization
 
 var particles = []; // create an empty array called particles
-var particleNum = 14*14; // var to store the number of particles
+var particleNum = 14 * 14; // var to store the number of particles
 
 var currentMode = 0; // variable to store the current visualization mode
 
 var cors = 'https://cors-anywhere.herokuapp.com/'; // CORS workaround (https://www.freecodecamp.org/forum/t/solved-having-trouble-getting-response-from-dark-sky-api/100653/5)
 var URI = 'https://api.darksky.net/forecast/'; // base URI for the API call
 var APIkey = '3a01d9d1f25578775e371597417f92f1'; // key for the API call (this will need to be hidden in the future)
-var exclude = 'exclude=minutely,hourly,daily,alerts,flags'; // exluding parts of the API call we don't need
+var exclude = 'exclude=minutely,hourly,daily,alerts,flags'; // exluding parts of the API call we don't need (minimize the data)
 var units = 'units=si'; // use the metric unit system
 var lat_long = '49.2827,-123.1207'; // latitude and longitude for Vancouver, BC
 
@@ -19,32 +20,36 @@ var API; // variable to store the API string
 
 function setup() {
     appDiv = select('#p5'); // selecting the div with the ID 'p5' and store it inside appDiv
+    
     createNewCanvas(); // create a new canvas
 
-    for(let i=0; i<particleNum; i++) { 
+    for (let i = 0; i < particleNum; i++) {
         particles.push(new Particle(i)); // create a new particle and push it into the particles array
     }
 
     API = cors + URI + APIkey + '/' + lat_long + '?' + units + '&' + exclude; // put together the API call
     loadData(); // load the data right away when the program starts
-    setInterval(loadData,120000); // set an interval which allws an API call every two minutes to update the weather data (this is based on 1000 free calls / day)
+    setInterval(loadData, 120000); // set an interval which allws an API call every two minutes to update the weather data (this is based on 1000 free calls / day)
     background(0); // set background to black initially, to avoid fade
 }
 
 function draw() {
-    background(0,10); // set background to black with 10/255 opacity
+    background(0, 10); // set background to black with 10/255 opacity
 
-    for(let i=0; i<particles.length; i++) { // cycle through all particles
+    for (let i = 0; i < particles.length; i++) { // cycle through all particles
         particles[i].show(); // call the show function of the current particle
         particles[i].update(); // call the update function of the current particle
     }
+
+
+    
 }
 
 function createNewCanvas() {
     appWidth = appDiv.size().width; // store the current width of 'appDiv' in 'appWidth'
     appHeight = appDiv.size().height; // store the current height of 'appDiv' in 'appHeight'
 
-    p5AppCanvas = createCanvas(appWidth,appHeight); // creating a new canvas and storing the result inside p5AppCanvas
+    p5AppCanvas = createCanvas(appWidth, appHeight); // creating a new canvas and storing the result inside p5AppCanvas
     p5AppCanvas.parent(appDiv); // assign the parent element of 'p5AppCanvas' to 'appDiv'
 }
 
@@ -55,23 +60,26 @@ function windowResized() {
 }
 
 function setMode(newMode) {
-    background(0) // refresh the background to black
-    for(let i=0; i<particles.length; i++) { // cycle through all particles
+    background(0); // refresh the background to black
+    for (let i = 0; i < particles.length; i++) { // cycle through all particles
         particles[i].setParticleMode(newMode); // call the setParticleMode function after the window has been resized
     }
 }
 
 function keyPressed() {
     console.log(keyCode); //
-    if(keyIsDown(49)) {
+    if (keyIsDown(49)) {
         currentMode = 0; // wait mode
     } else if (keyIsDown(50)) {
-        currentMode = 1; // wait mode
+        currentMode = 1; // sun mode
     } else if (keyIsDown(51)) {
-        currentMode = 2; // wait mode
+        currentMode = 2; // rain mode
     } else if (keyIsDown(52)) {
-        currentMode = 3; // wait mode
-    } else { 
+        currentMode = 3; // wind mode
+    }
+    else if (keyIsDown(53)) {
+        currentMode = 4; // cloudy mode
+    } else {
         currentMode = 0; // wait mode
     }
     setMode(currentMode); // update particles with currentMode
@@ -82,13 +90,44 @@ function loadData() {
 }
 
 function gotData(d) {
+    console.log(d);
     let weather = d.currently.icon; // read the current weather and store it inside weather (clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or partly-cloudy-night)
-    if (weather == 'cloudy') {
+    console.log(weather);
+
+    if (weather == 'sun') {
         setMode(1);
-    } else {
+    }
+    else if (weather == 'rain') {
+        setMode(2);
+    }
+    else if (weather == 'wind') {
+        setMode(3);
+    }
+    else if (weather == 'cloudy') {
+        setMode(4);
+    }
+    else {
         setMode(0);
     }
+
+
+   
+    let windSpeed = createP('windspeed : ' + d.currently.windSpeed); // create a variable for the windspeed text string and create a paragraph in it with the data
+    windSpeed.parent('windSpeedDiv'); //assign the windspeed data container to the existing div with the id windSpeed
+
+    let humidity= createP('humidity : '+ d.currently.humidity);
+    humidity.parent('humidityDiv');
+
+    let temperature = createP("temperature : " + d.currently.temperature);
+    temperature.parent('temperatureDiv');
+
+    let summary = createP('summary : '+d.currently.summary);
+    summary.parent('summaryDiv');
+
 }
+
+
+
 
 function error(e) {
     console.log(e);
